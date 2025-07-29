7fb7ac9cbccb8d8d81ed7facc7bb4a2044ee0831
loccal <loccal@gmail.com>
2025-07-28 23:13:15 -0300

    Cambios en Vite Config

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,30 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            // Quita el input de aquí, ya está definido en el plugin de Laravel
+            output: {
+                // Organiza los assets en carpetas específicas
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+            },
+        },
+        // Asegura que el manifest se genere en la raíz del directorio build
+        emptyOutDir: true,
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
